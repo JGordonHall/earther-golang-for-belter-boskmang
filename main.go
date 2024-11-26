2c64b7cd794285a6c090f868d07eb1f72b411b5d
JGordonHall <jarred.p.hall@gmail.com>
2024-11-25 19:18:50 -0800

    Golang

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// this is commented out
+func main() {
+	//fmt.Println("Hello World")
+	//var name string = "KodeKlode"
+	//var user string = "Harry"
+	//fmt.Print("Welcome to ", name, ",", user)
+
+	//fmt.Printf("Template string %s", Object args(s))
+	//var name string = "Gordon"
+	//var i int = 6
+	//fmt.Printf("Hey, %v! You are going to pass your Google Cloud Security Engineer exam in %d weeks time", name, i)
+	var i int
+	fmt.Printf("%d", i)
+}
