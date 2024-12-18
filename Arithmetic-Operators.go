0cf18998dd85cc0d4ca8818631c229e52e7ddb66
JGordonHall <jarred.p.hall@gmail.com>
2024-12-18 15:01:31 -0800

    Linux Push

---
+++ Arithmetic-Operators.go
@@ -0,0 +1,106 @@
+/*
+	Description: Arithmetic Operators in Go (Addition)
+
+package main
+
+import "fmt"
+
+	func main() {
+		var a, b string = "foo", "bar"
+		fmt.Println(a + b)
+	}
+
+Output: foobar
+
+//   Description: Arithmetic Operators in Go (Subtraction)
+
+	[Note: This code will not compile, as Go does not support subtraction of strings]
+
+package main
+
+import "fmt"
+
+	func main() {
+		var a, b string = "Kuh", "Blam"
+		fmt.Println(a - b)
+	}
+
+	Output: invalid operation: a - b (mismatched types string)
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b float64 = 79.02, 75.66
+	fmt.Printf("%.2f", a-b)
+}
+
+Output: 3.36
+
+// Description: Arithmetic Operators in Go (Multiplication)
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 11, 8
+	fmt.Println(a * b)
+}
+
+Output: 88
+
+// Description: Arithmetic Operators in Go (Division)
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 100, 10
+	fmt.Println(a / b)
+}
+
+Output: 10
+
+// Description: Arithmetic Operators in Go (Modulus)
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 24, 7
+	fmt.Println(a % b)
+}
+
+Output: 3
+
+// Description: Arithmetic Operators in Go (Increment)
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1
+	i++
+	fmt.Println(i)
+}
+
+Output: 2
+
+// Description: Arithmetic Operators in Go (Decrement)
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 4
+	i--
+	fmt.Println(i)
+}
+
+Output: 3 */
--- Multiple-Input-2.go
+++ Multiple-Input-2.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -19,3 +20,4 @@ func main() {
 	fmt.Println("b: ", b)
 
 }
+*\
