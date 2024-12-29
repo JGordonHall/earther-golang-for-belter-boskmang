c42d6435d07f6703766493dfe58cf56cd2662552
JGordonHall <jarred.p.hall@gmail.com>
2024-12-28 16:09:02 -0800

    Winter Break Updates

---
+++ Bitwise-Operators.go
@@ -0,0 +1,64 @@
+// Description: This is a example of Bitwise Operators in Golang. Here we are using AND (&) Bitwise operator. This operator takes two numbers as operands and does AND on every bit of two numbers. The result of AND is 1 only if both bits are 1. If either of the bits is 0, the result is 0.
+
+/*package main
+import "fmt"
+
+func main() {
+	var x, y int = 12, 25
+	z := x & y
+	fmt.Println(z)
+}
+Output: 8  This is the output of the code snippet
+
+// Description: This is a example of Bitwise Operators in Golang. Here we are using OR (|) Bitwise operator. This operator takes two numbers as operands and does OR on every bit of two numbers. The result of OR is 1 if either of the bits is 1. If both bits are 0, the result is 0.
+
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int = 12, 25
+	z := x | y
+	fmt.Println(z)
+}
+
+// Output: 29  This is the output of the code snippet
+
+// Description: This is a example of Bitwise Operators in Golang. Here we are using XOR (^) Bitwise operator. This operator takes two numbers as operands and does XOR on every bit of two numbers. The result of XOR is 1 if the bits are different. If both bits are the same, the result is 0.
+
+package main
+import "fmt"
+
+func main() {
+    var x, y int = 12 , 25
+    z := x  ^ y
+    fmt.Println(z)
+}
+// Output: 21  This is the output of the code snippet
+
+// Description: This is a example of Bitwise Operators in Golang. Here we are using left shift (<<) Bitwise operator. This operator takes two numbers as operands. The left operands value is moved left by the number of bits specified by the right operand.
+
+package main
+import "fmt"
+
+func main() {
+	var x int = 212
+	z := x << 1
+	fmt.Println(z)
+}
+
+// Output: 424  This is the output of the code snippet
+
+// Description: This is a example of Bitwise Operators in Golang. Here we are using right shift (>>) Bitwise operator. This operator takes two numbers as operands. The left operands value is moved right by the number of bits specified by the right operand.
+
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 212
+	z := x >> 2
+	fmt.Println(z)
+}
+
+// Output: 53  This is the output of the code snippet */
---
+++ Control-Flow.go
@@ -0,0 +1,48 @@
+// Description: This file contains the implementation of the control flow. Control Flow is the order in which the computer executes statements in a script. The control flow of a script is determined by the order in which the statements are written. The control flow of a script can be altered by using control flow statements. Control flow statements are used to alter the order in which the computer executes statements in a script. There are three types of control flow statements in Golang: if, for, and switch. The if statement is used to execute a block of code if a specified condition is true. The for statement is used to execute a block of code a specified number of times. The switch statement is used to execute a block of code based on the value of a variable. The control flow statements in Golang are similar to those in other programming languages, such as C and Java. However, Golang has some unique features that make it easier to write and read code. For example, Golang has a built-in error handling mechanism that makes it easy to handle errors in a script. Golang also has a built-in garbage collector that automatically frees up memory when it is no longer needed. Golang is a powerful and flexible programming language that is well-suited for writing scripts that require complex control flow.
+/*
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Happy"
+	if a == "Happy" {
+		fmt.Println(a)
+	}
+}
+// Output: Happy
+
+// Description: This is a example of if-else statement in Golang. The if statement is used to execute a block of code if a specified condition is true. The else statement is used to execute a block of code if the condition is false. In this example, we are using the if-else statement to check if the value of the variable "fruit" is "grapes". If the value of the variable "fruit" is "grapes", the code inside the if block is executed. If the value of the variable "fruit" is not "grapes", the code inside the else block is executed.
+
+package main
+
+import "fmt"
+
+func main() {
+	var fruit string = "grapes"
+	if fruit == "apples" {
+		fmt.Println("Fruit is apple")
+	} else {
+		fmt.Println("Fruit is not apple")
+	}
+}
+// Output: Fruit is not apple
+
+// Description: This is a example of an if-else if-else statement in Golang. The if statement is used to execute a block of code if a specified condition is true. The else statement is used to execute a block of code if the condition is false. The else if statement is used to execute a block of code if the condition is false. In this example, we are using the if-else if-else statement to check if the value of the variable "fruit" is "apples", "grapes", or "oranges". If the value of the variable "fruit" is "apples", the code inside the if block is executed. If the value of the variable "fruit" is "grapes", the code inside the else if block is executed. If the value of the variable "fruit" is "oranges", the code inside the else block is executed.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fruit := "grapes"
+	if fruit == "apples" {
+		fmt.Println("I Love Green Apples")
+	} else if fruit == "Orange" {
+		fmt.Println("Oranges are not apples")
+	} else {
+		fmt.Println("no appeitite")
+	}
+}
+// Output: no appeitite */
